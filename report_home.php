d7ad7d969fed04da78f8bb848e2c615ee50452c7
Mruczek <wozniak@mruczek.org>
2017-09-03 16:55:07 +0200

    Raport z wykresami

---
+++ report_home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>IoT Server</title>
+</head>
+<body style="background-color: dimgrey;">
+
+<div style="text-align: center; margin: auto; width: 100%;">
+    Temperatura:<br/>
+    <img alt="wykres" src="rrd_graph.php?s01;value;temp;end-24h"/><br/>
+    <img alt="wykres" src="rrd_graph.php?s02b;value;temp;end-24h"/><br/>
+    Ciśnienie atmosferyczne:<br/>
+    <img alt="wykres" src="rrd_graph.php?s02a;value;temp;end-24h"/><br/>
+</div>
+
+
+
+</body>
+</html>
