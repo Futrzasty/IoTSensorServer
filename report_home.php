9c6acd681e054053a181ab475083547d3293efaa
Mruczek <wozniak@mruczek.org>
2021-03-15 16:51:55 +0100

    Czujnik nr 10

--- report_home.php
+++ report_home.php
@@ -33,7 +33,8 @@ $kody  = array ("s01" => "Temperatura",
     "s08b" => "Stan baterii NRF Battery Sensor",
     "s09t" => "Temperatura wewnętrzna NRF Battery Sensor",
     "s09b" => "Stan baterii NRF Battery Sensor",
-);
+    "s10t" => "Temperatura wewnętrzna NRF Battery Sensor",
+    "s10b" => "Stan baterii NRF Battery Sensor",);
 ?>
 
 <div style="text-align: center; margin: auto; width: 100%; color: lightgray;">
